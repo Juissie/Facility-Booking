f76375f88e0e8a9e7260cabe59cffdec29d70a3b
liewjunjie <junjie.liew.2022@scis.smu.edu.com>
2023-09-03 15:59:39 +0800

    Initial Commit

---
+++ src/main/java/com/example/cs203g1t3/Cs203g1t3Application.java
@@ -0,0 +1,13 @@
+package com.example.cs203g1t3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs203g1t3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs203g1t3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cs203g1t3/Cs203g1t3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs203g1t3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs203g1t3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
