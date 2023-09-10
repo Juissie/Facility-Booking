342316e9001a71a44c2d49286c6cd93e5ebf2921
liewjunjie <junjie.liew.2022@scis.smu.edu.com>
2023-09-10 21:25:09 +0800

    Saving file to push for peer comments

--- backend/src/main/java/com/example/cs203g1t3/models/Account.java
+++ backend/src/main/java/com/example/cs203g1t3/models/Account.java
@@ -1,84 +1,48 @@
 package com.example.cs203g1t3.models;
 
 import java.time.LocalDateTime;
-import org.springframework.data.annotation.*;
-import jakarta.annotation.Generated;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
+import java.util.List;
+
+
+import jakarta.persistence.*;
+
+
+import lombok.*;
+
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 @Entity
+@Table(name = "accounts")
 public class Account {
 
     @Id 
-    @Generated(value = { "Id" })
-    private String userID;
+    @GeneratedValue (strategy = GenerationType.IDENTITY) 
+    Long userId;
 
-    
+
     private String password;
-    
-    private boolean accountStatus;
+
+    @Column(name = "last_active")
     private LocalDateTime lastActive;
 
     @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "address")
     private String address;
-    private String email;
+
+    @Column(name = "emailId")
+    private String emailId;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
 
+    public Account(String firstName,String lastName,String emailId){
 
-    
-     //setter
-    public void setUserID(String userID) {
-        this.userID = userID;
     }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public void setAccountStatus(boolean accountStatus) {
-        this.accountStatus = accountStatus;
-    }
-    public void setLastActive(LocalDateTime lastActive) {
-        this.lastActive = lastActive;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-
-
-
-    //getter
-    public String getUserID() {
-        return userID;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public boolean isAccountStatus() {
-        return accountStatus;
-    }
-    public LocalDateTime getLastActive() {
-        return lastActive;
-    }
-    public String getName() {
-        return name;
-    }
-    public Address getAddress() {
-        return address;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-    
 }
--- backend/src/main/java/com/example/cs203g1t3/models/Customer.java
+++ backend/src/main/java/com/example/cs203g1t3/models/Customer.java
@@ -2,9 +2,12 @@ package com.example.cs203g1t3.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import jakarta.persistence.OneToOne;
+
 public class Customer extends Account{
 
     @Autowired
+    @OneToOne
     private Account account;
 
     private long creditNumber;
