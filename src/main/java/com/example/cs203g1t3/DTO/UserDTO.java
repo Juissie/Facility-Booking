3cdaa4fe1d8d31af76c1ad90fd6e8aca35f2aca2
Jevon Tan <jevon.tan.2022@scis.smu.edu.sg>
2023-09-21 09:53:29 +0800

    Create frontend folder

---
+++ src/main/java/com/example/cs203g1t3/DTO/UserDTO.java
@@ -0,0 +1,16 @@
+package com.example.cs203g1t3.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private String username;
+    private String password;
+    private String email;
+}
