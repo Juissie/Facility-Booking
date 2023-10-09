681b8faade9c97a08c4de373946f65d26bdeb75b
weiqichua <143248754+weiqichua@users.noreply.github.com>
2023-10-09 15:40:51 +0800

    Implementation of facility

--- src/main/java/com/example/cs203g1t3/models/Facility.java
+++ src/main/java/com/example/cs203g1t3/models/Facility.java
@@ -1,8 +1,19 @@
 package com.example.cs203g1t3.models;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "facility")
@@ -12,10 +23,29 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+
+// LocalDateTime reference : 
+    // https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/time/package-summary.html
+
+
 public class Facility {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy =  GenerationType.IDENTITY)
     private Long facilityId;
-    
+    private String facilityType;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime openTime;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime endTime;
+    private boolean isCurrentlyBooked;
+    private String description;
+
+    public Facility(String facilityType, String description) {
+        this.facilityType = facilityType;
+        this.description = description;
+    }
+
+
+
 }
